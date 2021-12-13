cea9562d485ddac91bb1af0b668774207f2520c6
mroldev <mrolpierre@gmail.com>
2021-12-13 21:34:45 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>{{message}}</h1>
+</template>
+
+      <script>
+              export default  {
+                  data(){
+                      return{
+                          message :"Hello World"
+                      }
+                  }
+              }
+      </script>
+
+
+<style scoped>
+    h1{
+      color: aqua;
+      text-justify: center;
+      font-size: 50px;
+    }
+</style>
